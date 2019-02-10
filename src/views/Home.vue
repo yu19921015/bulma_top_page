0c26edaaa7fa2fd95077d48bdf5cfd26e5efc026
yu19921015 <yuu.koyama.3154@gmail.com>
2019-02-10 23:24:53 +0900

    checkoutできないためとりあえずのcommit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,21 +3,24 @@
     <Navigation></Navigation>
     <Introduction></Introduction>
     <Sponsors></Sponsors>
+    <GridSystem></GridSystem>
   </div>
 </template>
 
 <script>
-    // @ is an alias to /src
-    import Navigation from '@/components/Navigation.vue'
-    import Introduction from '@/components/Introduction.vue'
-    import Sponsors from '@/components/Sponsors.vue'
+  // @ is an alias to /src
+  import Navigation from '@/components/Navigation.vue'
+  import Introduction from '@/components/Introduction.vue'
+  import Sponsors from '@/components/Sponsors.vue'
+  import GridSystem from '@/components/GridSystem.vue'
 
-    export default {
-        name: 'home',
-        components: {
-            Navigation,
-            Introduction,
-            Sponsors
-        }
+  export default {
+    name: 'home',
+    components: {
+      Navigation,
+      Introduction,
+      Sponsors,
+      GridSystem
     }
+  }
 </script>
