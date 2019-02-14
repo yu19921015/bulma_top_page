a12484ffb6f16d9d0e4a545f476966bc67d11e97
yu19921015 <yuu.koyama.3154@gmail.com>
2019-02-14 23:29:06 +0900

    雛形作成

---
+++ src/components/NonCustomizedBulmaDemo.vue
@@ -0,0 +1,13 @@
+<template>
+  
+</template>
+
+<script>
+  export default {
+    name: "NonCustomizedBulmaDemo"
+  }
+</script>
+
+<style>
+
+</style>
---
+++ src/components/QuickToCustomize.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <NonCustomizedBulmaDemo></NonCustomizedBulmaDemo>
+    <CustomizedBulmaDemo></CustomizedBulmaDemo>
+  </div>
+</template>
+
+<script>
+  import NonCustomizedBulmaDemo from './NonCustomizedBulmaDemo'
+  import CustomizedBulmaDemo from './CustomizedBulmaDemo'
+
+  export default {
+    name: "QuickToCustomize",
+    components: {
+      NonCustomizedBulmaDemo,
+      CustomizedBulmaDemo
+    },
+    component: {
+      NonCustomizedBulmaDemo,
+      CustomizedBulmaDemo
+    }
+  }
+</script>
+
+<style>
+
+</style>
