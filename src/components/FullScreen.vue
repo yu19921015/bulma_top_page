0f685cc8acaccbd587c636c83e9c81a7454b1720
yu19921015 <yuu.koyama.3154@gmail.com>
2019-02-17 13:24:35 +0900

    デザイン崩れ修正

--- src/components/FullScreen.vue
+++ src/components/FullScreen.vue
@@ -1,23 +1,25 @@
 <template>
-  <div class="hero is-light is-fullheight">
-    <div class="hero-head">
-      <div class="container has-text-centered">
-        <div class="has-text-grey-light">This is always at the top</div>
-      </div>
-    </div>
-    <div class="hero-body">
-      <div class="container has-text-centered">
-        <div class="title">
-          <strong><span class="has-text-primary">Fullscreen</span> vertical centering</strong>
-        </div>
-        <div class="subtitle has-text-grey-light">
-          Include any content you want, it's always centered
+  <div class="fullscreen">
+    <div class="hero is-light is-fullheight">
+      <div class="hero-head">
+        <div class="container has-text-centered">
+          <div class="has-text-grey-light">This is always at the top</div>
         </div>
       </div>
-    </div>
-    <div class="hero-foot">
-      <div class="container has-text-centered">
-        <div class="has-text-grey-light">And this at the bottom</div>
+      <div class="hero-body">
+        <div class="container has-text-centered">
+          <div class="title">
+            <strong><span class="has-text-primary">Fullscreen</span> vertical centering</strong>
+          </div>
+          <div class="subtitle has-text-grey-light">
+            Include any content you want, it's always centered
+          </div>
+        </div>
+      </div>
+      <div class="hero-foot">
+        <div class="container has-text-centered">
+          <div class="has-text-grey-light">And this at the bottom</div>
+        </div>
       </div>
     </div>
   </div>
@@ -30,5 +32,7 @@
 </script>
 
 <style>
-
+  .fullscreen {
+    margin-top: 10rem;
+  }
 </style>
---
+++ src/components/SupportBulma.vue
@@ -0,0 +1,13 @@
+<template>
+  $END$
+</template>
+
+<script>
+  export default {
+    name: "SupportBulma"
+  }
+</script>
+
+<style scoped>
+
+</style>
